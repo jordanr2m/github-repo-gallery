70fd45526e6b39fb1a8b21161526ea749166226a
Jordan J <jordanr2m@gmail.com>
2023-10-29 14:25:19 -0700

    Finished second step and updated mobile formatting.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 const profileInfo = document.querySelector(".overview");
 const username = "jordanr2m";
 
+// List of repos
+const repoList = document.querySelector(".repo-list");
+
 const getGitData = async function () {
     const res = await fetch(`https://api.github.com/users/${username}`);
     const data = await res.json();
     // console.log(data); -> Shows all object properties
     displayData(data);
-}
+};
 // Must call this function in order for data to be pulled!
 getGitData();
 
@@ -26,4 +29,26 @@ const displayData = function (data) {
             <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div>`;
     profileInfo.append(userInfoDiv);
+    getRepos();
+};
+
+const getRepos = async function () {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const data = await res.json();
+    // console.log(data);
+    displayRepos(data);
 }
+// getRepos(); - Checking that we are pulling list of repos
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItems = document.createElement("li");
+        repoItems.classList.add("repo");
+        repoItems.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItems);
+    }
+    /* Can also use a forEach loop here instead: 
+        repos.forEach(function (repo) {
+            Same function  body
+        }) */
+};
