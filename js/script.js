cd8f0eea882371d61a7b523aeededecc457a7d7c
Jordan J <jordanr2m@gmail.com>
2023-10-30 13:24:47 -0700

    Finished step 3.

--- js/script.js
+++ js/script.js
@@ -5,16 +5,22 @@ const username = "jordanr2m";
 // List of repos
 const repoList = document.querySelector(".repo-list");
 
+// Repo info section - starting screen
+const allReposContainer = document.querySelector(".repos");
+// Individual repo data - secondary screen
+const repoData = document.querySelector(".repo-data");
+
+
 const getGitData = async function () {
-    const res = await fetch(`https://api.github.com/users/${username}`);
-    const data = await res.json();
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
     // console.log(data); -> Shows all object properties
-    displayData(data);
+    displayUserData(data);
 };
 // Must call this function in order for data to be pulled!
 getGitData();
 
-const displayData = function (data) {
+const displayUserData = function (data) {
     const userInfoDiv = document.createElement("div");
     userInfoDiv.classList.add("user-info");
     // Populate the div with the needed elements. Use template literal
@@ -33,11 +39,11 @@ const displayData = function (data) {
 };
 
 const getRepos = async function () {
-    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
-    const data = await res.json();
-    // console.log(data);
-    displayRepos(data);
-}
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    // console.log(repoData);
+    displayRepos(repoData);
+};
 // getRepos(); - Checking that we are pulling list of repos
 
 const displayRepos = function (repos) {
@@ -47,8 +53,45 @@ const displayRepos = function (repos) {
         repoItems.innerHTML = `<h3>${repo.name}</h3>`;
         repoList.append(repoItems);
     }
-    /* Can also use a forEach loop here instead: 
-        repos.forEach(function (repo) {
-            Same function  body
-        }) */
+    /* Can also use a forEach loop here. Use for of instead because we don't need to grab the index number. */
+};
+
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText;
+        // console.log(repoName);
+        getRepoData(repoName);
+    }
+});
+
+const getRepoData = async function (repoName) {
+    const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await fetchInfo.json();
+    // console.log(repoInfo);
+
+    // Grab languages - fetch can target specific attributes of the data
+    const fetchLanguages = await fetch(repoInfo.languages_url);
+    const languageData = await fetchLanguages.json();
+    // console.log(languageData);
+    // Make a list of languages
+    const languages = [];
+    for (const language in languageData) {
+        languages.push(language);
+    }
+
+    displayRepoData(repoInfo, languages);
+};
+
+const displayRepoData = function (repoInfo, languages) {
+    repoData.innerHTML = ""; // clear section each time
+    repoData.classList.remove("hide");
+    allReposContainer.classList.add("hide");
+    const repoDiv = document.createElement("div");
+    repoDiv.innerHTML = 
+        `<h3>Name: ${repoInfo.name}</h3>
+        <p>Description: ${repoInfo.description}</p>
+        <p>Default Branch: ${repoInfo.default_branch}</p>
+        <p>Languages: ${languages.join(", ")}</p>
+        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    repoData.append(repoDiv);
 };
